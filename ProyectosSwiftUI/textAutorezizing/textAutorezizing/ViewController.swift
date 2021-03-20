4ef4e904acbc6d007ca17ff7c46515e802416880
Juan Luis Martinez Rocha <jl.canlo@yahoo.com.mx>
2021-03-20 10:18:33 -0600

    Texto autoajustado

---
+++ ProyectosSwiftUI/textAutorezizing/textAutorezizing/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  textAutorezizing
+//
+//  Created by JUAN MARTINEZ ROCHA on 20/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var nombreEmpleado: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        nombreEmpleado.text="Francisco Ramìrez de la Mora Cordero"
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
